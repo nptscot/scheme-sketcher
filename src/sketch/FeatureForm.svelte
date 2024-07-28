5ab1e6e066c2714ac6436d30ba8c16c484c70375
Dustin Carlino <dabreegster@gmail.com>
2024-07-28 12:22:59 +0100

    Let users auto-fill route name

--- src/sketch/FeatureForm.svelte
+++ src/sketch/FeatureForm.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-  import { TextInput, Select, TextArea } from "govuk-svelte";
+  import { FormElement, SecondaryButton, Select, TextArea } from "govuk-svelte";
   import { type Config } from "scheme-sketcher-lib/config";
   import type { FeatureProps, Schemes } from "scheme-sketcher-lib/draw/types";
   import type { Writable } from "svelte/store";
   import type { NptFeature, NptScheme } from "./types";
+  import { routeTool } from "scheme-sketcher-lib/draw/stores";
+  import { v4 as uuidv4 } from "uuid";
 
   export let cfg: Config<NptFeature, NptScheme>;
   export let gjSchemes: Writable<Schemes<NptFeature, NptScheme>>;
@@ -12,9 +14,36 @@
 
   // @ts-expect-error Hide svelte-check warning
   let _ = id;
+
+  // Sets the intervention name to "From {road1 and road2} to {road3 and
+  // road4}". Only meant to be useful for routes currently.
+  function autoFillName() {
+    try {
+      props.name = $routeTool!.inner.routeNameForWaypoints(props.waypoints);
+    } catch (e) {
+      window.alert(`Couldn't auto-name route: ${e}`);
+    }
+  }
+
+  let nameId = uuidv4();
 </script>
 
-<TextInput label="Name" bind:value={props.name} />
+<FormElement label="Name" id={nameId}>
+  <!-- Only LineStrings can be auto-named, and length_meters being set is the simplest proxy for that -->
+  {#if props.length_meters}
+    <div class="govuk-hint">
+      If transcribing from a document, use the route name there if possible
+    </div>
+  {/if}
+
+  <input type="text" class="govuk-input" id={nameId} bind:value={props.name} />
+
+  {#if props.length_meters}
+    <SecondaryButton on:click={() => autoFillName()} disabled={!$routeTool}>
+      Auto-fill with street names
+    </SecondaryButton>
+  {/if}
+</FormElement>
 
 <Select
   label="Scheme"
